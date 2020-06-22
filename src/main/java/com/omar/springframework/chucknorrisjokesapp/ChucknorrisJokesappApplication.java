9f534ad2ce4403add9693b1d02c367ff9d8e316c
Omar Morales Ortega <morales.ortega.omar@gmail.com>
2020-06-22 16:35:28 -0500

    Spring initializr

---
+++ src/main/java/com/omar/springframework/chucknorrisjokesapp/ChucknorrisJokesappApplication.java
@@ -0,0 +1,13 @@
+package com.omar.springframework.chucknorrisjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChucknorrisJokesappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChucknorrisJokesappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omar/springframework/chucknorrisjokesapp/ChucknorrisJokesappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omar.springframework.chucknorrisjokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChucknorrisJokesappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
